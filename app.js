e05fe5d152b7252f9a1ed80e2da77e454a0087c7
Luis G. D.P <106504056+LuisGDP1711@users.noreply.github.com>
2022-11-16 21:50:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const express = require ('express')
+const mongoose = require('mongoose')
 const app = express ()
 
-//let {people} = require ('./data')
-app.use(express.json())  //required
+const PORT = 5600   // SERVER PORT
+
+const DB_USER = 'test'
+const DB_PASSWORD = 'test'
+
+app.use(express.json())
 
 
 const people = require('./datos/people')
 app.use('/api/personas/general', people)
 
-
-const id = require('./datos/id')
-app.use('/api/personas/id', id)
-
-const nombre = require('./datos/nombre')
-app.use('/api/personas/nombre', nombre)
+const age = require('./datos/age')
+app.use('/api/personas/edad', age)
 
 const apellidos = require('./datos/Apellidos')
 app.use('/api/personas/apellidos', apellidos)
 
 const dni = require('./datos/dni')
-app.use('/api/personas/dni', dni)
+app.use('/api/personas/DNI', dni)
 
-const age = require('./datos/age')
-app.use('/api/personas/edad', age)
+const nombre = require('./datos/nombre')
+app.use('/api/personas/nombre', nombre)
 
 
-app.listen(5600, () => {
-    console.log('Server esta en el puerto 5600...')
+const id = require('./datos/id')
+app.use('/api/personas/id', id)
+
+
+
+app.all('*', (req,res) => {
+    res.status(404).send('Direccion no encontrada')
 })
+
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@labcluster.0s2vqmw.mongodb.net/DATA_PROYECT?retryWrites=true&w=majority`
+    ).then(() => {
+        console.log('Conectado al MONGODB')
+        app.listen(PORT, () => console.log(`Server esta en el puerto ${PORT}...`))
+    })
+    .catch((err) => {
+        console.log(err)
+    })
