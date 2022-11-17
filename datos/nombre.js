38a46dab977afc7a4d9b7a7c09e4a9d6c27fa731
Luis G. D.P <106504056+LuisGDP1711@users.noreply.github.com>
2022-11-16 21:56:54 -0500

    Update nombre.js

--- datos/nombre.js
+++ datos/nombre.js
@@ -1,70 +1,91 @@
 const express = require('express')
 const router_1 = express.Router()
+const Person = require('../model/Person')
 
-let { people_data } = require('../data')
-
-
-
-router_1.get('/', (req, res) => {
-    const simplename = people_data.map((nombre) => {
-        const {id, name} = nombre
-        return {id, name}
-        })
-    res.status(200).json ({ success: true, data: simplename})
+// muestra en la ruta principal todos los nombres de las personas
+router_1.get('/', async(req, res) => {
+    try {
+        const simple_nombre = await Person.find({}, {_id: 1, name: 1})
+        res.status(200).json({success: true ,simple_nombre})
+    } catch (error) {
+        res.status(500).json({success: false , error:error })
+    }
 })
 
-router_1.get('/:nameID', (req, res) => {
-    console.log(req.params)
-    const {nameID} = req.params
-    const singlename = people_data.find(
-        name => name.id === Number(nameID))
-    if (!singlename) {
-        return res.status(404).send('Nombre no encontrado')
-    }
-    res.json(singlename)
-})
-
-
-router_1.post('/', (req,res) => {
-    const {name} = req.body
-
-    if (!name) { 
-        return res
-        .status(404)
-        .json({success: false, msg: 'Proveer nombre'})
-    }
-    res.status(201).send( { success: true, data: [...people_data, name]})
-})
-
-
-router_1.put('/:id', (req, res) => {
-    const {id} = req.params
-    const {name} = req.body
-    const person = people_data.find ((person) => person.id === Number(id))
-    if (!person) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `No hay persona con id: ${id}`})
-    }
-    const new_name = people_data.map(person => {
-        if (person.id === Number(id)) {
-            person.name = name
+// muestra el nombre segun el ID
+router_1.get('/:nameID', async (req, res) => {
+    const nameID = req.params.nameID
+    try {
+        const person = await Person.findOne({_id: nameID})
+        if (!person) {
+            res.status(422).json({success: false ,msg: `No hay persona con id: ${nameID}`})
+            return
         }
-        return person
-    })
-    res.status(201).send ( {success: true, data: new_name})
+        res.status(200).json({success: true, person})
+    } catch (error) {
+        res.status(500).json({success: false , error: error})
+    }
 })
 
-router_1.delete('/:id', (req, res) => {
-    const {id} = req.params
-    const person = people_data.find ((person) => person.id === Number(id))
-    if (!person) {
+// muestra si hay personas con un nombre en especifico
+router_1.get('/buscar/:nombre', async (req, res) => {
+    const nombre = req.params.nombre
+    if (isNaN(Number(nombre)) === false) {
         return res
-            .status(404)
-            .json({ success: false, msg: `No hay persona con id: ${id}`})
+            .status(400)
+            .json({ success: false, msg: 'Tipo de dato erroneo, ingrese un dato tipo string'})
     }
-    const new_name = people_data.filter(person => person.id !== Number(id))
-    return res.status(200).send ( {success: true, data: new_name})
+    try {
+        const person1 = await Person.findOne({name: nombre})
+        if (!person1) {
+            res.status(422).json({success: false ,msg: `No hay usuarios con el nombre: ${nombre}`})
+            return
+        }
+        const person = await Person.find({name: nombre})
+        res.status(200).json({success: true, person})
+    } catch (error) {
+        res.status(500).json({success: false , error: error})
+    }
+})
+
+// actualiza el nombre de una persona segun el id
+router_1.patch('/:id', async (req, res) => {
+    const id = req.params.id
+    const {name} = req.body
+    const person = {
+        name,
+    }
+    if (typeof name !== "string") {
+        return res
+            .status(400)
+            .json({ success: false, msg: 'Tipo de dato erroneo, ingrese un dato tipo string'})
+    }
+    try {
+        const updatePerson = await Person.updateOne({_id: id}, person)
+        if (updatePerson.matchedCount === 0) {
+            res.status(422).json({success: false ,msg: `No hay persona con id: ${id}`})
+            return
+        }
+        res.status(200).json({success: true, person})
+    } catch (error) {
+        res.status(500).json({success: false ,error: error})
+    }
+})
+
+// elimina una persona segun el ID
+router_1.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    const person = await Person.findOne({ _id: id })
+    if (!person) { 
+        res.status(422).json({ success: false, msg: `No hay persona con id: ${id}`})
+        return
+    }
+    try {
+        await Person.deleteOne({_id: id})
+        res.status(200).json({success: true ,msg: 'Usuario removido'})
+    } catch (error) {
+        res.status(500).json({ success: false, error: error })
+    }
 })
 
 
