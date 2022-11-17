5ed1bc4c3399545e4b640ac66a5d8a1dc0b70f3a
Luis G. D.P <106504056+LuisGDP1711@users.noreply.github.com>
2022-11-16 21:55:39 -0500

    Update dni.js

--- datos/dni.js
+++ datos/dni.js
@@ -1,71 +1,98 @@
 const express = require('express')
 const router_1 = express.Router()
+const Person = require('../model/Person')
 
-let { people_data } = require('../data')
-
-
-router_1.get('/', (req, res) => {
-    const simpleDNI = people_data.map((surna) => {
-        const {id, DNI} = surna
-        return {id, DNI}
-        })
-    res.status(200).json ({ success: true, data: simpleDNI})
+// muestra los DNI de las personas
+router_1.get('/', async(req, res) => {
+    try {
+        const simple_DNI = await Person.find({}, {_id: 1, DNI: 1})
+        res.status(200).json({success: true ,simple_DNI})
+    } catch (error) {
+        res.status(500).json({success: false , error:error })
+    }
 })
 
-router_1.get('/:dniID', (req, res) => {
-    console.log(req.params)
-    const {dniID} = req.params
-    const singleDNI = people_data.find(
-        surna => surna.id === Number(dniID))
-    if (!singleDNI) {
-        return res.status(404).send('DNI no encontrado')
-    }
-    res.json(singleDNI)
-})
-
-
-router_1.post('/', (req,res) => {
-    const {DNI} = req.body
-
-    if (!DNI) { 
-        return res
-        .status(404)
-        .json({success: false, msg: 'Proveer DNI'})
-    }
-    res.status(201).send( { success: true, data: [...people_data, DNI]})
-})
-
-
-router_1.put('/:id', (req, res) => {
-    const {id} = req.params
-    const {DNI} = req.body
-    const person = people_data.find ((person) => person.id === Number(id))
-    if (!person) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `No hay persona con id: ${id}`})
-    }
-    const new_DNI = people_data.map(person => {
-        if (person.id === Number(id)) {
-            person.DNI = DNI
+// muestra la informacion de una persona segun el ID
+router_1.get('/:DNI_ID', async (req, res) => {
+    const DNI_ID = req.params.DNI_ID
+    try {
+        const person = await Person.findOne({_id: DNI_ID})
+        if (!person) {
+            res.status(422).json({success: false ,msg: `No hay persona con id: ${DNI_ID}`})
+            return
         }
-        return person
-    })
-    res.status(201).send ( {success: true, data: new_DNI})
-})
-
-router_1.delete('/:id', (req, res) => {
-    const {id} = req.params
-    const DNI_person = people_data.find ((person) => person.id === Number(id))
-    if (!DNI_person) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `No hay persona con id: ${id}`})
+        res.status(200).json({success: true, person})
+    } catch (error) {
+        res.status(500).json({success: false , error: error})
     }
-    const new_DNI = people_data.filter(person => person.id !== Number(id))
-    return res.status(200).send ( {success: true, data: new_DNI})
 })
 
+// muestra la informacion de una persona segun el DNI
+router_1.get('/buscar/:DNI', async (req, res) => {
+    const DNI = Number(req.params.DNI)
+    if (isNaN(DNI)) {
+        return res
+            .status(400)
+            .json({ success: false, msg: 'Tipo de dato erroneo, ingrese un dato tipo numerico'})
+    }
+    try {
+        const person = await Person.findOne({DNI: DNI})
+        if (!person) {
+            res.status(422).json({success: false ,msg: `No hay usuarios con el DNI: ${DNI}`})
+            return
+        }
+        res.status(200).json({success: true, person})
+    } catch (error) {
+        res.status(500).json({success: false , error: error})
+    }
+})
 
+// actualiza el DNI de una persona segun el id
+router_1.patch('/:DNI_ID', async (req, res) => {
+    const DNI_ID = req.params.DNI_ID
+    const {DNI} = req.body
+    const person = {
+        DNI,
+    }
+    if (typeof DNI !== "number") {
+        return res
+            .status(400)
+            .json({ success: false, msg: 'Tipo de dato erroneo, ingrese un dato tipo numerico'})
+    }
+    const updatePerson = await Person.updateOne({DNI: DNI}, person)
+        //console.log(updatePerson)
+    if (updatePerson.matchedCount === 1) {
+        res.status(422).json({success: false ,msg: `Ya existe una persona con el DNI: ${DNI}`})
+        return
+    }
+    try {
+        const updatePerson = await Person.updateOne({_id: DNI_ID}, person)
+        //console.log(updatePerson)
+        if (updatePerson.matchedCount === 0) {
+            res.status(422).json({success: false ,msg: `No hay persona con id: ${id}`})
+            return
+        }
+        res.status(200).json({success: true, person})
+    } catch (error) {
+        res.status(500).json({success: false ,error: error})
+    }
+    
+})
+
+// elimina una persona segun el DNI
+router_1.delete('/:DNI', async (req, res) => {
+    const DNI = req.params.DNI
+    const person = await Person.findOne({ DNI: DNI })
+    if (!person) { 
+        res.status(422).json({ success: false, msg: `No hay persona con el DNI: ${DNI}`})
+        return
+    }
+    try {
+        await Person.deleteOne({DNI: DNI})
+        res.status(200).json({success: true ,msg: 'Usuario removido'})
+    } catch (error) {
+        res.status(500).json({ success: false, error: error })
+    }
+})
 
 module.exports = router_1
